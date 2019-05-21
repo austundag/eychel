6ba93ea205517d3da134f0f119e0ca538bc4a38d
Afsin Ustundag <afsin@amida.com>
2019-05-21 11:10:36 -0400

    some more testing

--- src/main/java/com/supmountain/eychel/Application.java
+++ src/main/java/com/supmountain/eychel/Application.java
@@ -4,7 +4,9 @@ import javax.jms.ConnectionFactory;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
-import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
@@ -16,17 +18,22 @@ public class Application {
 		CamelContext context = new DefaultCamelContext();
 
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-		context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+		JmsComponent jmsComponent = JmsComponent.jmsComponentAutoAcknowledge(connectionFactory);
+		context.addComponent("test-jms", jmsComponent);
 
-		ConnectionFactory connectionFactory2 = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-		JmsComponent jmsComponent = JmsComponent.jmsComponentAutoAcknowledge(connectionFactory2);
-		context.addComponent("test-jms2", jmsComponent);
+		// context.addComponent("test-jms2", jmsComponent);
 
 		context.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
 				// from("test-jms:queue:test.queue").to("file://test");
-				from("test-jms:queue:test.queue").to("test-jms:queue:test.queue");
+				from("test-jms:topic:test.queue").multicast().to("stream:out", "direct:fily");
+				from("direct:fily").process(new Processor() {
+					public void process(Exchange exchange) {
+						Message in = exchange.getIn();
+						in.setBody(in.getBody(String.class) + " World!\n");
+					}
+				}).to("file://test?fileName=target.txt&fileExist=Append");
 			}
 		});
 
@@ -34,23 +41,23 @@ public class Application {
 		context.start();
 
 		for (int i = 0; i < 10; i++) {
-			template.sendBody("test-jms:queue:test.queue", "Test Message: " + i);
+			template.sendBody("test-jms:topic:test.queue", "Test Message: " + i);
 		}
 
 		Thread.sleep(1000);
 
-		ConsumerTemplate consumerTemplate = context.createConsumerTemplate(30);
+		// ConsumerTemplate consumerTemplate = context.createConsumerTemplate(30);
 
-		for (int i = 0; i < 20; i++) {
-			System.out.println("XXXX" + i);
-			Object object = consumerTemplate.receiveBody("test-jms2:queue:test.queue");
-			System.out.println(i);
-			if (object != null) {
-				System.out.println(object.toString());
-			} else {
-				System.out.println("null");
-			}
-		}
+		// for (int i = 0; i < 20; i++) {
+		// System.out.println("XXXX" + i);
+		// Object object = consumerTemplate.receiveBody("test-jms:queue:test2.queue2");
+		// System.out.println(i);
+		// if (object != null) {
+		/// System.out.println(object.toString());
+		// } else {
+		/// System.out.println("null");
+		// }
+		// }
 
 		context.stop();
 	}
